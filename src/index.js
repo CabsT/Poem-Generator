7cfc99f2746f6fd1356a386de0023c13fc358ca0
CabsT <133862761+CabsT@users.noreply.github.com>
2024-05-31 18:37:32 +0200

    Integrated a poem generation with SheCodes AI API and display using Typewriter.js
    - Added `generatePoem` function to handle form submission and generate a poem using SheCodes AI API.
    - Configured the `generatePoem` function to send a request to the API with user instructions and context.
    - Displayed a loading message while the poem is being generated.
    - Utilized Typewriter.js in the `displayPoem` function to animate the display of the generated poem.
    - Updated the form submission event listener to trigger the `generatePoem` function.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 
-function displayPoem (event){
-  event.preventDefault();
+function displayPoem (response){
   new Typewriter("#poem", {
-    strings: ["La tombe dit à la rose"],
+    strings: response.data.answer,
     autoStart: true,
     delay: 50,
     pauseFor: 500,
@@ -15,6 +14,23 @@ function displayPoem (event){
 
 }
 
+function generatePoem(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector(".search");
+  let apiKey = "2046c535afeb092fo82f1d306d8a2b2t";
+  let context =
+    "You are a romantic Poem expert and love to write short poems. You mission is to generate a 6 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Include a title to the poem within a <h3>. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
+  let prompt = `User instructions: Generate a French poem about ${instructionsInput.value}`;
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let poemElement = document.querySelector("#poem");
+  poemElement.innerHTML = `<div class="generating">⏳ Generating a French poem about ${instructionsInput.value}</div>`;
+
+  axios.get(apiURL).then(displayPoem);
+}
+
+
 
 let poemElement = document.getElementById('poem-searchForm');
-poemElement.addEventListener('submit', displayPoem);
+poemElement.addEventListener('submit', generatePoem);
