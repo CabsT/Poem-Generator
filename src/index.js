24abe44ea4ec65d78d65bf7ab224fc1a7e387217
Charné Teichert <133862761+CabsT@users.noreply.github.com>
2025-07-23 20:05:49 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ function generatePoem(event) {
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   let poemElement = document.querySelector("#poem");
-  poemElement.innerHTML = `<div class="generating">⏳ Generating a French poem about ${instructionsInput.value}</div>`;
+  poemElement.innerText = `<div class="generating">⏳ Generating a French poem about ${instructionsInput.value}</div>`;
 
   axios.get(apiURL).then(displayPoem);
 }
